b2c91fa9287c46709cbdf62a79bbfb34461301cd
daksh963655 <daksh.kumar@aayaninfotech.com>
2025-01-08 13:44:55 +0530

    added

--- src/component/SignIn/SignIn.jsx
+++ src/component/SignIn/SignIn.jsx
@@ -21,7 +21,7 @@ function Signin() {
     setSuccessMessage("");
 
     try {
-      const response = await fetch("http://44.196.64.110:9006/api/auth/login", {
+      const response = await fetch("http://localhost:9006/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
